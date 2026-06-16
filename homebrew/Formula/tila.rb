7de1fe00eda3010d9cc5b50ba747a76d1a3cb8ad
Dawid Leszczyński <dawid.leszczynski@hey.com>
2026-06-16 11:44:35 +0200

    chore(release): bump public version to 0.2.7

--- homebrew/Formula/tila.rb
+++ homebrew/Formula/tila.rb
@@ -1,27 +1,27 @@
 class Tila < Formula
   desc "State-and-coordination engine for multi-machine agentic work"
   homepage "https://github.com/davebream/tila"
-  version "0.2.6"
+  version "0.2.7"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/davebream/tila/releases/download/v0.2.6/tila-darwin-arm64"
+      url "https://github.com/davebream/tila/releases/download/v0.2.7/tila-darwin-arm64"
       sha256 "7c9985ae21b795c8891f7a706fe07d7c264f519a252fdb70b7bdce184a561ff2"
     end
     on_intel do
-      url "https://github.com/davebream/tila/releases/download/v0.2.6/tila-darwin-x64"
+      url "https://github.com/davebream/tila/releases/download/v0.2.7/tila-darwin-x64"
       sha256 "b3aac961ac18a3a63ff4f9c94232da20c3ad7ec385af7c7449561d03dfc5211e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/davebream/tila/releases/download/v0.2.6/tila-linux-arm64"
+      url "https://github.com/davebream/tila/releases/download/v0.2.7/tila-linux-arm64"
       sha256 "8368383145cc82cf41bc83b1547b6e06ecb8e4fed76114ab24b2c891e1c17faf"
     end
     on_intel do
-      url "https://github.com/davebream/tila/releases/download/v0.2.6/tila-linux-x64"
+      url "https://github.com/davebream/tila/releases/download/v0.2.7/tila-linux-x64"
       sha256 "f53dc0feb8f8340fdaf38ad2ab73e6ff6018b1d4bc293c1ad9c94d5bf80514d7"
     end
   end
